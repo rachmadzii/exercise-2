9afb6e0cca55bf34860ef8e3d196b37d436b12bf
Rachmadzii <rachmadzima4@gmail.com>
2022-04-08 10:02:46 +0700

    exercise: config eslint

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import SearchGif from "./pages/SearchGif";
 import Trending from "./pages/Trending";
 
 function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Switch>
-                <Route path="/trending">
-                    <Trending />
-                </Route>
-                <Route path="/search">
-                    <SearchGif />
-                </Route>
-            </Switch>
-        </Router>
-    );
+  return (
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/trending" exact>
+          <Trending />
+        </Route>
+        <Route path="/search" exact>
+          <SearchGif />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Searchbar/index.js
+++ src/components/Searchbar/index.js
@@ -4,47 +4,47 @@ import { setQuery } from "../../searchSlice";
 import { getImages } from "../../utils/fetchApi";
 
 export default function SearchBar({ onSuccess }) {
-    const query = useSelector((state) => state.search.query);
-    const dispatch = useDispatch();
+  const query = useSelector((state) => state.search.query);
+  const dispatch = useDispatch();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-        const images = await getImages(query, 12);
-        onSuccess(images.data);
-    };
+    const images = await getImages(query, 12);
+    onSuccess(images.data);
+  };
 
-    const handleChange = async (e) => {
-        dispatch(setQuery(e.target.value));
+  const handleChange = async (e) => {
+    dispatch(setQuery(e.target.value));
 
-        const images = await getImages(query, 12);
-        onSuccess(images.data);
-    };
+    const images = await getImages(query, 12);
+    onSuccess(images.data);
+  };
 
-    const clearSearch = () => {
-        dispatch(setQuery(""));
-        onSuccess([]);
-    };
+  const clearSearch = () => {
+    dispatch(setQuery(""));
+    onSuccess([]);
+  };
 
-    return (
-        <div className="search-wrapper">
-            <form onSubmit={handleSubmit} className="form-search">
-                <div className="form-group">
-                    <input
-                        onChange={handleChange}
-                        type="text"
-                        value={query}
-                        placeholder="Search gif..."
-                        required
-                    />
-                    <button type="submit" className="btn-primary">
-                        Search
-                    </button>
-                </div>
-            </form>
-            <button className="btn btn-text" onClick={clearSearch}>
-                Clear Search
-            </button>
+  return (
+    <div className="search-wrapper">
+      <form onSubmit={handleSubmit} className="form-search">
+        <div className="form-group">
+          <input
+            onChange={handleChange}
+            type="text"
+            value={query}
+            placeholder="Search gif..."
+            required
+          />
+          <button type="submit" className="btn-primary">
+            Search
+          </button>
         </div>
-    );
+      </form>
+      <button className="btn btn-text" onClick={clearSearch}>
+        Clear Search
+      </button>
+    </div>
+  );
 }
